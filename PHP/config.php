c456912926a232dd2baff76d3f3d0251d8f3c012
Liridon <vondony@hotmail.com>
2020-03-06 16:41:48 +0100

    DB Stuff

---
+++ PHP/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Config file, should be included by all page controllers
+ */
+
+// Enable verbose output of error (or include from config.php)
+error_reporting(-1);              // Report all type of errors
+ini_set("display_errors", 1);     // Display all errors
+
+// Create an array with the connection details
+$dsn = [
+    "dsn"       => "mysql:host=127.0.0.1;port=3306;dbname=websoft;charset=UTF8",  
+    "username"  => "root",
+    "password"  => "root",
+];
