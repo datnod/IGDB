993b5926bf41d4aa550eedf6505ed80355cc4426
OskarForssman <Oskar.forssman@hotmail.com>
2020-03-07 15:29:07 +0100

    projekt

--- PHP/toplist.php
+++ PHP/toplist.php
@@ -1,54 +1,45 @@
-<?php $headerTitle = "Top 10"; include 'view/header.php'; 
-
-/**
- * Top list that will contain top rated games.
- */
-
-$topinfo = "Top 10 Games: ";
-
+<?php  $headerTitle = "Search DB"; include 'view/header.php';
+$topinfo = "Top Games: ";
 echo "<h2>" . $topinfo . "</h2>";
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "mydb";
 
-require "config.php";
-require "connect.php";
-
-// Get incoming values
-$top = $_GET["search"] ?? null;
-$like = "%$top%";
-
-if ($top) {
-    // Connect to the database
-    $db = connectDatabase($dsn);
-
-    // Prepare and execute the SQL statement
-    $sql = <<<EOD
-SELECT
-    *
-FROM mydb.IGDB
-;
-EOD;
-    $stmt = $db->prepare($sql);
-    $stmt->execute([$top, $like]);
-
-    // Get the results as an array with column names as array keys
-    $res = $stmt->fetchAll();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT * FROM mydb.IGDB ORDER BY Review DESC; ";
+$result = $conn->query($sql);
+
+
+if ($result && $result->num_rows) {
+    echo "<table><tr>    
+            <th>Title</th>
+            <th>Description</th>
+            <th>Genre</th>
+            <th>Rating!</th>
+            <th>Release Data</th>
+            <th>Developer/Publisher</th>
+        </tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>
+        <td>".$row["Title"]."</td>
+        <td>".$row["Description"]."</td>
+        <td>".$row["Genre"]."</td>
+        <td>".$row["Review"]."</td>
+        <td>".$row["Release Data"]."</td>
+        <td>".$row["Developer"]."</td>
+        </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
-
- 
-<table>
-    
-
-
-
-    
-       
-       
-       
-           
-         
-         
-       
-            
-       
-</table>        
--- PHP/view/header.php
+++ PHP/view/header.php
@@ -12,6 +12,6 @@
           | <a href="index.php">HOME</a> |
           <a href="search.php">SEARCH</a> |
           <a href="about.php">ABOUT</a> |
-          <a href="toplist.php">TOP 10</a> |
+          <a href="toplist.php">TOP</a> |
         </nav>
 </header>
