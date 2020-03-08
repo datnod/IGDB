80ad7fbe635a96ee2492fb6bef9d80a353dd87de
OskarForssman <Oskar.forssman@hotmail.com>
2020-03-08 16:04:05 +0100

    update highscore

--- PHP/toplist.php
+++ PHP/toplist.php
@@ -3,24 +3,45 @@
 require "config.php";
 require "connect.php";
 
+
+
+
+
 // Connect to the database
 $db = connectDatabase($dsn);
 
 // Prepare and execute the SQL statement
+
+
+
 $sql = <<<EOD
 SELECT
     *
 FROM mydb.IGDB
 ORDER BY
     review
-DESC   
+DESC 
     ;
 EOD;
+
+
+
+
+    
+
     $stmt = $db->prepare($sql);
     $stmt->execute();
-
     // Get the results as an array with column names as array keys
     $res = $stmt->fetchAll();
+
+
+    $sql = "UPDATE mydb.IGDB
+    SET Review = ?
+    WHERE idIGDB = ?;";
+    $stmt = $db->prepare($sql);
+   
+$test=1;
+$test2=2;
 
 ?>
 
@@ -39,6 +60,7 @@ EOD;
             <th>Review</th>
             <th>Release</th>
             <th>Publisher</th>
+            <th>Rate!</th>
         </tr>
     <?php foreach ($res as $row) : ?>
         <tr>
@@ -48,9 +70,11 @@ EOD;
             <td><?= $row["Genre"] ?></td>
             <td><?= $row["Review"] ?></td>
             <td><?= $row["Release Data"] ?></td>
-            <td><?= $row["Developer/Publisher"] ?></td>
+            <td><?= $row["Developer"] ?></td>
+            <td><button type="RATE" onclick="$stmt->execute($test,$test2);" value="">RATE</button></td>
         </tr>
     <?php endforeach; ?>
+
 
 </table>
     
