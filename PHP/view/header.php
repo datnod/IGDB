2604babbb7eac455a885fe1ef8ba285110ae33d7
OskarForssman <Oskar.forssman@hotmail.com>
2020-03-17 16:18:05 +0100

    test

--- PHP/view/header.php
+++ PHP/view/header.php
@@ -11,11 +11,23 @@
 <div class="container indigo topBotomBordersOut">
 <div class ="login-container">
   <ul class="nav navbar-nav navbar-right">
-  <form action="">
-  <input type ="text" placeholder="Username" name="username">
-  <input type ="text" placeholder="Password" name="password">
-  <button type="submit">Login</button>
+  
+  <form method="post">
+  <input type ="account" placeholder="Username" name="username">
+  <input type ="password" placeholder="Password" name="password">
+  <button type="submit" name="submit">Login</button>
   </form>
+
+<?php
+
+if(isset($_POST['submit'])){
+  $username=$_POST['username'];
+  $password=$_POST['password'];
+ echo ("$username");
+ echo ("$password");
+}
+?>
+
 </div>
 <a href="index.php">Home</a>
   <a href="about.php">About</a>
