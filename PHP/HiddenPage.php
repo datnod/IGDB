3de560b8409f01683eaa890aefa83cf52f02ad40
Liridon <vondony@hotmail.com>
2020-03-18 02:24:13 +0100

    HiddenPage Structure [Under work]

--- PHP/HiddenPage.php
+++ PHP/HiddenPage.php
@@ -1,13 +1,48 @@
 <?php
 session_start();
 
+/*
+*HTML divs booleans for create,delete,update shown if true, by default these are false.
+*/
+
+$createBool = false;
+$updateBool = false;
+$deleteBool  = false;
+
 if($_SESSION["validUser"] === true){
-  echo "Welcome to CRUD";
-  var_dump($_SESSION['validUser']);
+  
   session_destroy();
+
 }else{ 
     header("Location: index.php");
     exit;
 }
 
 ?>
+
+<html >
+
+  <body>
+
+    <div id="create" <?php if ($createBool===false){?>style="display:none"<?php } ?>>
+      <?php 
+        echo "Create Operation";
+      ?>
+    </div>
+
+    <div id="delete" <?php if ($deleteBool===false){?>style="display:none"<?php } ?>>
+      <?php 
+        echo "Delete Operation";
+      ?>
+    </div>
+
+    <div id="update" <?php if ($updateBool===false){?>style="display:none"<?php } ?>>
+      <?php 
+        echo "Update Operation";
+      ?>
+    </div>
+
+  </body>
+
+</html>
+
